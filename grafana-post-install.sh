a3194949663afa4c4487b68a6e8a23964c515aec
Nicola Venuti <venuti@amazon.com>
2020-10-14 11:33:46 +0200

    Add the license header everywhere

--- grafana-post-install.sh
+++ grafana-post-install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+#
+#
 
 #source the AWS ParallelCluster profile
 . /etc/parallelcluster/cfnconfig
