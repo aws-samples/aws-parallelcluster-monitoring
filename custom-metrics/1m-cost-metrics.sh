2797f77905a1682e1c265b2aed2e29403d406404
Nicola Venuti <venuti@amazon.com>
2020-11-16 16:39:13 +0100

    Fix the compute node cost metric script. Broken after the PC release 2.9.0 (multi-queue support). Thanks again to @nonatofabio for reporting it.

--- custom-metrics/1m-cost-metrics.sh
+++ custom-metrics/1m-cost-metrics.sh
@@ -11,12 +11,23 @@
 
 export AWS_DEFAULT_REGION=$cfn_region
 aws_region_long_name=$(python /usr/local/bin/aws-region.py $cfn_region)
-computeInstanceType=$(aws cloudformation describe-stacks --stack-name $stack_name --region $cfn_region | jq -r '.Stacks[0].Parameters | map(select(.ParameterKey == "ComputeInstanceType"))[0].ParameterValue')
+github_repo=$(echo ${cfn_postinstall_args}| cut -d ',' -f 1 )
+setup_command=$(echo ${cfn_postinstall_args}| cut -d ',' -f 2 )
+monitoring_dir_name=$(basename -s .git ${github_repo})
+queues=$(/opt/slurm/bin/sinfo --noheader -O partition  | sed 's/\*//g')
+cluster_config_file="/home/${cfn_cluster_user}/${monitoring_dir_name}/parallelcluster-setup/cluster-config.json"
 
-compute_node_h_price=$(aws pricing get-products \
+compute_nodes_total_cost=0
+
+for queue in $queues; do 
+
+  instance_type=$(cat "${cluster_config_file}" \
+                        | jq -r --arg queue $queue '.cluster.queue_settings | to_entries[].value.compute_resource_settings | to_entries[] | select(.key==$queue).value.instance_type')
+
+  compute_node_h_price=$(aws pricing get-products \
     --region us-east-1 \
     --service-code AmazonEC2 \
-    --filters 'Type=TERM_MATCH,Field=instanceType,Value='$computeInstanceType \
+    --filters 'Type=TERM_MATCH,Field=instanceType,Value='$instance_type \
               'Type=TERM_MATCH,Field=location,Value='"${aws_region_long_name}" \
               'Type=TERM_MATCH,Field=preInstalledSw,Value=NA' \
               'Type=TERM_MATCH,Field=operatingSystem,Value=Linux' \
@@ -26,28 +37,24 @@ compute_node_h_price=$(aws pricing get-products \
     --query 'PriceList' \
     | jq -r '.terms.OnDemand | to_entries[] | .value.priceDimensions | to_entries[] | .value.pricePerUnit.USD')
 
-#ebs_volume_id=$(aws ec2 describe-instances     --instance-ids $computeInstanceId \
-#              | jq -r '.Reservations | to_entries[].value | .Instances | to_entries[].value | .BlockDeviceMappings | to_entries[].value | .Ebs.VolumeId' \
-#              | tail -1) #remove this tail
+  ebs_cost_gb_month=$(aws --region us-east-1 pricing get-products \
+    --service-code AmazonEC2 \
+    --query 'PriceList' \
+    --output text \
+    --filters 'Type=TERM_MATCH,Field=location,Value='"${aws_region_long_name}" \
+              'Type=TERM_MATCH,Field=productFamily,Value=Storage' \
+              'Type=TERM_MATCH,Field=volumeApiName,Value=gp2' \
+    | jq -r '.terms.OnDemand | to_entries[] | .value.priceDimensions | to_entries[] | .value.pricePerUnit.USD')
 
-#ebs_volume_type=$(aws ec2 describe-volumes --volume-ids $ebs_volume_id | jq -r '.Volumes | to_entries[].value.VolumeType')
-#ebs_volume_iops=$(aws ec2 describe-volumes --volume-ids $ebs_volume_id | jq -r '.Volumes | to_entries[].value.Iops')
-ebs_volume_size=$(aws cloudformation describe-stacks --stack-name $stack_name --region $cfn_region | jq -r '.Stacks[0].Parameters | map(select(.ParameterKey == "ComputeRootVolumeSize"))[0].ParameterValue')
+  total_num_compute_nodes=$(/opt/slurm/bin/sinfo --noheader  --partition=$queue  | egrep  -v "idle~" | awk '{sum += $4} END {print sum}')
 
-#check if volumeApiName can chane in the future, for now "gp2" is hardcoded
-ebs_cost_gb_month=$(aws --region us-east-1 pricing get-products \
-  --service-code AmazonEC2 \
-  --query 'PriceList' \
-  --output text \
-  --filters 'Type=TERM_MATCH,Field=location,Value='"${aws_region_long_name}" \
-          'Type=TERM_MATCH,Field=productFamily,Value=Storage' \
-          'Type=TERM_MATCH,Field=volumeApiName,Value=gp2' \
-  | jq -r '.terms.OnDemand | to_entries[] | .value.priceDimensions | to_entries[] | .value.pricePerUnit.USD')
+  ebs_volume_size=$(aws cloudformation describe-stacks --stack-name $stack_name --region $cfn_region | jq -r '.Stacks[0].Parameters | map(select(.ParameterKey == "ComputeRootVolumeSize"))[0].ParameterValue')
+  compute_ebs_volume_cost=$(echo "scale=2; $ebs_cost_gb_month * $total_num_compute_nodes * $ebs_volume_size / 720" | bc)
+  compute_nodes_cost=$(echo "scale=2; $total_num_compute_nodes * $compute_node_h_price" | bc)
+  
+  compute_nodes_total_cost=$(echo "scale=2; $compute_nodes_total_cost + $compute_nodes_cost" | bc)
 
-total_num_compute_nodes=$(/opt/slurm/bin/sinfo -O "nodes" --noheader)
-compute_ebs_volume_cost=$(echo "scale=2; $ebs_cost_gb_month * $total_num_compute_nodes * $ebs_volume_size / 720" | bc)
-compute_nodes_cost=$(echo "scale=2; $total_num_compute_nodes * $compute_node_h_price" | bc)
+done 
 
-
-echo "ebs_compute_cost $compute_ebs_volume_cost" | curl --data-binary @- http://127.0.0.1:9091/metrics/job/cost
-echo "compute_nodes_cost $compute_nodes_cost" | curl --data-binary @- http://127.0.0.1:9091/metrics/job/cost
+echo "ebs_compute_cost $compute_ebs_volume_cost"    | curl --data-binary @- http://127.0.0.1:9091/metrics/job/cost
+echo "compute_nodes_cost $compute_nodes_total_cost" | curl --data-binary @- http://127.0.0.1:9091/metrics/job/cost
