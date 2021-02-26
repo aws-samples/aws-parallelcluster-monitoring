e0080fc3d8caf1422cbbfaebf3687141bad1c30d
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2021-02-26 15:25:23 -0500

    Add files via upload

---
+++ post_install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#
+
+# Env variable
+. /etc/parallelcluster/cfnconfig
+
+monitoring_url=$(echo ${cfn_postinstall_args}| cut -d ',' -f 1 )
+monitoring_dir_name=$(echo ${cfn_postinstall_args}| cut -d ',' -f 2 )
+monitoring_tarball="${monitoring_dir_name}.tar.gz"
+setup_command=$(echo ${cfn_postinstall_args}| cut -d ',' -f 3 )
+monitoring_home="/home/${cfn_cluster_user}/${monitoring_dir_name}"
+
+case ${cfn_node_type} in
+	MasterServer)
+		touch /home/centos/tonto
+		cat >/home/centos/stultus
+		echo '${cfn_postinstall_args}' >> /home/centos/tonto
+		echo '${monitoring_dir_name}' >> /home/centos/tonto
+		echo '${monitoring_tarball}' >> /home/centos/tonto
+		wget ${monitoring_url} -O ${monitoring_tarball}
+		mkdir -p ${monitoring_home}
+		tar xvf ${monitoring_tarball} - C ${monitoring_home} --strip-components 1
+	;;
+	ComputeFleet)
+
+	;;
+esac
+
+#
+bash -x "${monitoring_home}/parallelcluster-setup/${setup_command}" >/tmp/monitoring-setup.log 2>&1
+exit
