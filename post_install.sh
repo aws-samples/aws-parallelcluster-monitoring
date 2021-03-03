edf3dc425b59a6796b14e9665a611cd5b34154a3
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2021-03-02 21:17:28 -0500

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -13,19 +13,10 @@ monitoring_home="/home/${cfn_cluster_user}/${monitoring_dir_name}"
 
 case ${cfn_node_type} in
 	MasterServer)
-		touch /home/centos/tonto
-		cat >/home/centos/stultus
-		echo ${cfn_postinstall_args} >> /home/centos/tonto
-		echo ${monitoring_dir_name} >> /home/centos/tonto
-		echo ${monitoring_tarball} >> /home/centos/tonto
-		echo ${monitoring_home} >> /home/centos/tonto
-		echo >> /home/centos/tonto
-		echo ${monitoring_url} >> /home/centos/tonto
 		cd /home/centos
 		wget ${monitoring_url} -O ${monitoring_tarball}
 		mkdir -p ${monitoring_home}
 		tar xvf ${monitoring_tarball} -C ${monitoring_home} --strip-components 1
-		echo ${cfn_base_os} >> /home/centos/tonto
 	;;
 	ComputeFleet)
 
