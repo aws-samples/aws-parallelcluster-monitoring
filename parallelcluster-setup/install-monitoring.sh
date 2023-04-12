8954fc22ab16ed1d0ea4a1d2e66dbf3d684532dd
Sean Smith <seaam@amazon.com>
2023-04-12 08:30:10 -0700

    Simplify Deployment args
    Signed-off-by: Sean Smith <seaam@amazon.com>

--- parallelcluster-setup/install-monitoring.sh
+++ parallelcluster-setup/install-monitoring.sh
@@ -18,7 +18,7 @@ usermod -a -G docker $cfn_cluster_user
 curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-monitoring_dir_name=${cfn_postinstall_args[1]}
+monitoring_dir_name=aws-parallelcluster-monitoring
 monitoring_home="/home/${cfn_cluster_user}/${monitoring_dir_name}"
 
 echo "$> variable monitoring_dir_name -> ${monitoring_dir_name}"
